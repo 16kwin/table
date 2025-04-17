5337147bd1860286cfe27539f05ccacc83c6242c
16kwin <DKM_PSVU@mail.ru>
2025-04-17 19:48:15 +0500

    версия изменнеая по красоте

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Secondtable from './components/secondtable/secondtable';
 import Thirdtable from './components/thirdtable/thirdtable';
 import Fourtable from './components/fourtable/fourtable';
 const contentMap = {
-  content1: <Table />,
-  content2: <Secondtable />,
-  content3: <Thirdtable />,
-  content4: <Fourtable/>
+  content2: <Table />,
+  content3: <Secondtable />,
+  content4: <Thirdtable />,
+  content1: <Fourtable/>
 };
 function App() {
   const [activeContent, setActiveContent] = useState('content1');
--- src/components/header.js
+++ src/components/header.js
@@ -4,10 +4,10 @@ import '../styles/header.css';
 function Header({ onButtonClick }) {
   return (
     <div className="button-container">
-        <button onClick={() => onButtonClick('content1')}>Доска производственного анализа</button>
-        <button onClick={() => onButtonClick('content2')}>Сводная ведомость выроботки по сотрудникам</button>
-        <button onClick={() => onButtonClick('content3')}>Сводная ведомость по потерям производственного времени</button>
-        <button onClick={() => onButtonClick('content4')}>Депо</button>
+        <button onClick={() => onButtonClick('content1')}>Депо</button>
+        <button onClick={() => onButtonClick('content2')}>Доска производственного анализ</button>
+        <button onClick={() => onButtonClick('content3')}>Сводная ведомость выроботки по сотрудникам</button>
+        <button onClick={() => onButtonClick('content4')}>Сводная ведомость по потерям производственного времени</button>
     </div>
   );
 }
